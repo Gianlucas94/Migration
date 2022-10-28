eabb58da435768434672246c74a4ca254faf7c0c
Gianlucas94 <giandoscp@hotmail.com>
2022-10-28 18:22:52 -0300

    Download do arquivo DNS

--- migracao.sh
+++ migracao.sh
@@ -29,7 +29,7 @@ mkdir /home/$userName/temp/linux/
 cd /home/$userName/temp/linux/
 wget --no-check-certificate --content-disposition https://github.com/Gianlucas94/Migration/blob/main/UEMS_LinuxAgent.bin?raw=true
 wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Gianlucas94/Migration/main/serverinfo.json
-wget --no-check-certificate --content-disposition 
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Gianlucas94/Migration/main/dns.txt
 chmod +x UEMS_LinuxAgent.bin
 ./UEMS_LinuxAgent.bin
 
