2c3e96942307bba95d489f475f4374f9d726e2e6
Gianlucas94 <giandoscp@hotmail.com>
2022-10-31 13:56:43 -0300

    Loading

--- migracao2.sh
+++ migracao2.sh
@@ -7,12 +7,12 @@ userName=$USER
 tmpLinuxDir="/home/${userName}/temp/linux"
 
 main() {
-    echo "Instalando systemd..."
+    log_status "Instalando systemd..."
     if ! apt-get install systemd; then
         exit_error "Falha ao instalar systemd"
     fi
 
-    echo "Verificando tipo de dispositivo..."
+    log_status "Verificando tipo de dispositivo..."
     case "${deviceType}" in
     "N")
         deviceType="LT"
@@ -23,14 +23,14 @@ main() {
     esac
 
     NovoHostName="ENCSABCAM${deviceType}${Patrimonio}"
-    echo "Novo dispositivo: ${NovoHostName}"
+    log_status "Novo dispositivo: ${NovoHostName}"
 
-    echo "Alterando hostname..."
+    log_status "Alterando hostname..."
     if ! hostnamectl set-hostname "$NovoHostName" >/dev/null; then
         exit_error "Falha ao alterar hostname: ${NovoHostName}"
     fi
 
-    echo "Desinstalando KACE e McAfee..."
+    log_status "Desinstalando KACE e McAfee..."
     if ! sudo bash /opt/McAfee/agent/scripts/uninstall.sh; then
         exit_error "Falha ao desinstalar McAfee"
     fi
@@ -39,7 +39,7 @@ main() {
         exit_error "Falha ao desinstalar McAfee"
     fi
 
-    echo "Instalando Manage Engine..."
+    log_status "Instalando Manage Engine..."
     if mkdir -p "${tmpLinuxDir}"; then
         cd "${tmpLinuxDir}" || exit
     else
@@ -51,40 +51,47 @@ main() {
     links[1]="https://raw.githubusercontent.com/Gianlucas94/Migration/main/serverinfo.json"
     links[2]="https://raw.githubusercontent.com/Gianlucas94/Migration/main/dns.txt"
 
-    echo Baixando arquivos de configuração...
+    log_status Baixando arquivos de configuração...
     for link in "${!links[@]}"; do
         if ! wget --no-check-certificate --content-disposition "${links[$link]}"; then
             exit_error "Falha ao baixar arquivo do link: ${links[$link]}"
         fi
     done
 
-    echo "Tranformando arquivo UEMS_LinuxAgent.bin em executável..."
+    log_status "Tranformando arquivo UEMS_LinuxAgent.bin em executável..."
     if ! chmod +x UEMS_LinuxAgent.bin; then
         exit_error "Falha ao transformar arquivo UEMS_LinuxAgent.bin em executável"
     fi
 
-    echo "Executando UEMS_LinuxAgent.bin"
+    log_status "Executando UEMS_LinuxAgent.bin"
     if ! ./UEMS_LinuxAgent.bin; then
         exit_error "Falha ao executar UEMS_LinuxAgent.bin"
     fi
 
-    echo "Verificando se o ZScaler e o Defender foram instalados"
+    log_status "Verificando se o ZScaler e o Defender foram instalados"
     until [ -d /opt/zscaler ] && [ -d /opt/microsoft ]; do
-        sleep 5
+        spinner="/|\\-/|\\-"
+        while :; do
+            for i in $(seq 0 7); do
+                echo -n "${spinner:$i:1}"
+                echo -en "\010"
+                sleep 1
+            done
+        done
     done
-    echo "ZScaler e Defender instalados" >/dev/null
+    exit_sucess "ZScaler e Defender instalados" >/dev/null
 
-    echo "Resolvendo o problema do DNS..."
+    log_status "Resolvendo o problema do DNS..."
     if ! cat dns.txt >/etc/nsswitch.conf; then
         exit_error "Falha ao escrever no arquivo: /etc/nsswitch.conf"
     fi
 
-    echo "Reiniciando gerenciador de rede..."
+    log_status "Reiniciando gerenciador de rede..."
     if ! systemctl restart network-manager; then
         exit_error "Falha ao reiniciar gerenciador de rede"
     fi
 
-    echo "Apagando pasta Temp"
+    log_status "Apagando pasta Temp"
     if ! rm -rf /home/${userName}/temp/linux; then
         exit_error "Falha ao deletar a pasta Temp"
     fi
@@ -106,6 +113,13 @@ exit_error() {
     local color_off="\033[0m"
     echo -e "\n${red}${message}${color_off}\n"
     exit 1
+}
+
+log_status() {
+    local message="$1"
+    local yellow="\033[0;33m"
+    local color_off="\033[0m"
+    echo -e "\n${yellow}${message}${color_off}\n"
 }
 
 main
