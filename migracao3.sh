31ad9ce55d0c45a65fd273938341f2fe11a20171
Gianlucas94 <giandoscp@hotmail.com>
2022-11-03 19:01:15 -0300

    Fix bold on finish message

--- migracao3.sh
+++ migracao3.sh
@@ -116,7 +116,7 @@ main() {
 
 exit_success() {
     local message="$1"
-    local green="\033[1;32"
+    local green="\033[1;32m"
     local color_off="\033[0m"
     local EndTime=$(date '+%H:%M:%S')
     echo -e "\n${green}${message}${color_off}\n"
