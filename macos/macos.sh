24e84a8770455e55f7a5846d1950e3feb5b2af3f
Gianlucas94 <giandoscp@hotmail.com>
2022-11-10 12:17:35 -0300

    Fix curl

--- macos/macos.sh
+++ macos/macos.sh
@@ -67,7 +67,7 @@ main() {
 
     log_step "Baixando arquivos de configuração..."
     for link in "${!links[@]}"; do
-        if ! curl -o "${links[$link]}"; then
+        if ! curl -O "${links[$link]}"; then
             exit_fatal "Falha ao baixar arquivo do link: ${links[$link]}"
         fi
     done
